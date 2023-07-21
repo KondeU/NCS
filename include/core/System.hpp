6e3660ecacbb9c7390836b2169199998f207c086
Deu.K <370242479@qq.com>
2023-07-22 00:08:46 +0800

    feat: Registry换成拥有Relation的模板泛化后，System也需要结合对应的Relation变成模板类

--- include/core/System.hpp
+++ include/core/System.hpp
@@ -4,17 +4,18 @@
 
 namespace au::ncs {
 
+template <typename Relation>
 class System {
 public:
     virtual void OnCreate() = 0;
     virtual void OnDestroy() = 0;
 
-    virtual void OnPrepare(Registry& registry) = 0;
-    virtual void OnShutdown(Registry& registry) = 0;
+    virtual void OnPrepare(Registry<Relation>& registry) = 0;
+    virtual void OnShutdown(Registry<Relation>& registry) = 0;
 
-    virtual void OnPrepTick(Registry& registry, float delta) = 0;
-    virtual void OnProcTick(Registry& registry, float delta) = 0;
-    virtual void OnPostTick(Registry& registry, float delta) = 0;
+    virtual void OnPrepTick(Registry<Relation>& registry, float delta) = 0;
+    virtual void OnProcTick(Registry<Relation>& registry, float delta) = 0;
+    virtual void OnPostTick(Registry<Relation>& registry, float delta) = 0;
 };
 
 }
