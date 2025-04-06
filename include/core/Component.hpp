8b83fb7741fdefa45984facd7d98c52cf0932298
Deu.K <370242479@qq.com>
2025-04-07 00:09:57 +0800

    skipFields模式的ComponentBuffer实现FindFree&TryFree

--- include/core/Component.hpp
+++ include/core/Component.hpp
@@ -21,12 +21,8 @@ struct ComponentStorage {
 
 template <typename Component>
 struct ComponentBuffer : ComponentStorage {
-    static constexpr size_t EachBlockFieldsCount = 4;
-    static constexpr size_t EachFieldUnitsCount = 32;
-    static constexpr size_t EachBlockUnitsCount = EachFieldUnitsCount * EachBlockFieldsCount;
-
     struct SkipFields final {
-        uint32_t fields[EachBlockFieldsCount]{};
+        uint32_t fields[4]{};
 
         inline bool IsAllUsed() const noexcept
         {
@@ -48,9 +44,9 @@ struct ComponentBuffer : ComponentStorage {
             fields[field] &= ~(1 << bit);
         }
 
-        inline bool FindFreeBit(int& field, int& bit)
+        inline bool FindFreeBit(int& field, int& bit) const
         {
-            for (field = 0; field < 4; field++) { // EachBlockFieldsCount = 4
+            for (field = 0; field < 4; field++) {
                 bit = FindZeroBit(fields[field]);
                 if (bit >= 0) {
                     return true;
@@ -59,7 +55,7 @@ struct ComponentBuffer : ComponentStorage {
             return false;
         }
 
-        static inline static int FindZeroBit(uint32_t bits)
+        static inline int FindZeroBit(uint32_t bits)
         {
             return (bits == ~0U) ? -1 :
             [](uint32_t inverted) {
@@ -69,7 +65,7 @@ struct ComponentBuffer : ComponentStorage {
                 #elif defined(__GNUC__) || defined(__clang__)
                 return __builtin_ctz(inverted);
                 #else // Software implementation.
-                for (int i = 0; i < 32; i++) { // EachFieldUnitsCount = 32
+                for (int i = 0; i < 32; i++) {
                     if ((inverted & (1U << i)) != 0) {
                         return i;
                     }
@@ -103,44 +99,43 @@ struct ComponentBuffer : ComponentStorage {
 
     struct Block final {
         SkipFields skipFields;
-        Unit units[EachBlockUnitsCount]{};
+        Unit units[4 * 32]{}; // uint32_t[4]
 
         inline bool IsFull() const noexcept
         {
-            return fields.IsAllUsed();
+            return skipFields.IsAllUsed();
         }
 
         inline bool IsEmpty() const noexcept
         {
-            return fields.IsAllFree();
+            return skipFields.IsAllFree();
         }
 
-        inline int FindUnitIndex(const Unit* unit)
+        inline Unit* FindFree()
         {
-            //TODO
+            int field = 0, bit = 0;
+            if (!skipFields.FindFreeBit(field, bit)) {
+                return nullptr; // Not found free bit.
+            }
+            skipFields.MarkUsedBit(field, bit);
+            return &units[field * 32 + bit];
         }
 
-        inline void ForEach()
+        inline bool TryFree(Unit* unit)
         {
-            //TODO
+            size_t elements = unit - &units[0];
+            if (!(elements < 4 * 32)) {
+                return false; // Not in this block.
+            }
+            int field = elements / 32;
+            int bit   = elements % 32;
+            skipFields.MarkFreeBit(field, bit);
+            return true;
         }
     };
 
-
     std::list<Block> blocks; // Container.
     std::unordered_map<Node, Unit*> finder;
-
-    void TrimBlocks()
-    {
-        auto iter = blocks.begin() + 1;
-        while (iter != blocks.end()) {
-            if (*iter.IsEmpty()) {
-                iter = blocks.erase(iter);
-            } else {
-                iter++;
-            }
-        }
-    }
 
     ComponentBuffer()
     {
@@ -154,40 +149,51 @@ struct ComponentBuffer : ComponentStorage {
 
     void* GetComponent(Node node) override
     {
-        auto component = components.find(node);
-        if (component == components.end()) {
+        auto unit = finder.find(node);
+        if (unit == finder.end()) {
             return nullptr;
         }
-        return component->second;
+        return unit->second->ComponentPointer();
     }
 
     void* AddComponent(Node node) override
     {
-        auto& component = components[node];
-        if (component) {
-            return component;
-        }
-
+        //auto& component = components[node];
+        //if (component) {
+        //    return component;
+        //}
     }
 
     bool RemoveComponent(Node node) override
     {
-        return (components.erase(node) > 0);
+        //return (components.erase(node) > 0);
     }
 
     std::vector<Node> GetNodes() const override
     {
-        std::vector<Node> nodes;
-        nodes.reserve(components.size());
-        for (const auto& component : components) {
-            nodes.emplace_back(component.first);
-        }
-        return nodes;
+        //std::vector<Node> nodes;
+        //nodes.reserve(components.size());
+        //for (const auto& component : components) {
+        //    nodes.emplace_back(component.first);
+        //}
+        //return nodes;
     }
 
     size_t GetSize() const override
     {
-        return components.size();
+        return finder.size();
+    }
+
+    void TrimBlocks()
+    {
+        auto iter = blocks.begin() + 1;
+        while (iter != blocks.end()) {
+            if (*iter.IsEmpty()) {
+                iter = blocks.erase(iter);
+            } else {
+                iter++;
+            }
+        }
     }
 };
 
